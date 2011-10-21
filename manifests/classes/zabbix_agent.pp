cf7ea324299d6be820aaf7398a1f1f5721ca4b93
Tom Barber <tom@analytical-labs.com>
2011-10-21 12:13:22 +0100

    blah

---
+++ manifests/classes/zabbix_agent.pp
@@ -0,0 +1,54 @@
+class zabbix::agent {
+
+ 	$zabbix_config_dir = "/etc/zabbix"
+    $zabbix_agentd_conf = "$zabbix_config_dir/zabbix_agentd.conf"
+    $zabbix_register_sh = "$zabbix_config_dir/zabbix_register.sh"
+
+    package {
+        "zabbix-agent":
+            ensure => installed;
+        "curl":
+            ensure => installed,
+            before => File[$zabbix_register_sh];
+    }
+
+    service {
+        "zabbix-agent":
+            enable => true,
+            ensure => running,
+            hasstatus => true,
+            require => Package["zabbix-agent"]
+    }
+
+    file {
+        $zabbix_config_dir:
+            ensure => directory,
+            owner => root,
+            group => root,
+            mode => 0755,
+            require => Package["zabbix-agent"];    
+
+        $zabbix_register_sh:
+            owner => root,
+            group => root,
+            mode => 0700,
+            content => template("zabbix/zabbix_register.sh.erb"),
+            require => Package["zabbix-agent"];
+
+        $zabbix_agent_conf:
+            owner => root,
+            group => root,
+            mode => 0644,
+            content => template("zabbix_agent_conf.erb"),
+            require => Package["zabbix-agent"];
+
+        $zabbix_agentd_conf:
+            owner => root,
+            group => root,
+            mode => 0644,
+            content => template("zabbix/zabbix_agentd.conf.erb"),
+            require => Package["zabbix-agent"];
+    }
+
+    exec { $zabbix_register_sh:  }
+}
