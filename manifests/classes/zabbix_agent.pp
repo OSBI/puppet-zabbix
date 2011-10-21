99eafb7ec4560ef567d7c9794544c7e9af83bfa4
Tom Barber <tom@analytical-labs.com>
2011-10-21 12:49:18 +0100

    agent

--- manifests/classes/zabbix_agent.pp
+++ manifests/classes/zabbix_agent.pp
@@ -3,7 +3,7 @@ class zabbix::agent {
  	$zabbix_config_dir = "/etc/zabbix"
     $zabbix_agentd_conf = "/etc/zabbix/zabbix_agentd.conf"
     $zabbix_register_sh = "/etc/zabbix/zabbix_register.sh"
-
+	$zabbix_agent_conf = "/etc/zabbix/zabbix_agent.conf"
     package {
         "zabbix-agent":
             ensure => installed;
