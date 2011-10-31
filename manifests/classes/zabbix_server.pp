8e966aac62a263ff227aa692cc9761d2f38c8eff
Tom Barber <tom@analytical-labs.com>
2011-10-31 08:28:05 +0000

    fix indents

--- manifests/classes/zabbix_server.pp
+++ manifests/classes/zabbix_server.pp
@@ -1,62 +1,62 @@
 class zabbix::server {
 
-include apache
-include php
-include php::apache
-include mysql::server
+  include apache
+  include php
+  include php::apache
+  include mysql::server
 
-package { ["zabbix-server-mysql", "zabbix-frontend-php"]:
-	ensure => present,
-#	require => Service["mysqld"],
-	}
-    
-    file {
-        "/etc/zabbix/dbconfig.php":
-            ensure  => present,
-            content => template("zabbix/dbconfig.php.erb");
-        "/etc/zabbix/zabbix_server.d/":
-            ensure => directory,
-            owner  => "zabbix",
-            group  => "zabbix";
-        "/etc/zabbix/alertscripts":
-            ensure => directory,
-            owner  => "zabbix",
-            group  => "zabbix";
-        "/etc/zabbix/externalscripts":
-            ensure => directory,
-            owner  => "zabbix",
-            group  => "zabbix";
-        "/etc/zabbix/zabbix_server.conf":
-        	ensure => present,
-        	content => template("zabbix/zabbix_server_conf.erb");
+  package { ["zabbix-server-mysql", "zabbix-frontend-php"]:
+    ensure => present,
+    #	require => Service["mysqld"],
     }
 
-	mysql::database{"zabbix":
-  		ensure   => present,
-  		require => Class["mysql::server"]
-	}
+    file {
+      "/etc/zabbix/dbconfig.php":
+        ensure  => present,
+        content => template("zabbix/dbconfig.php.erb");
+      "/etc/zabbix/zabbix_server.d/":
+        ensure => directory,
+        owner  => "zabbix",
+        group  => "zabbix";
+      "/etc/zabbix/alertscripts":
+        ensure => directory,
+        owner  => "zabbix",
+        group  => "zabbix";
+      "/etc/zabbix/externalscripts":
+        ensure => directory,
+        owner  => "zabbix",
+        group  => "zabbix";
+      "/etc/zabbix/zabbix_server.conf":
+        ensure => present,
+        content => template("zabbix/zabbix_server_conf.erb");
+    }
+
+    mysql::database{"zabbix":
+      ensure   => present,
+      require => Class["mysql::server"]
+    }
 
 
-	mysql::rights{"zabbix rights":
-  		ensure   => present,
-  		database => "zabbix",
-  		user     => "zabbix",
-  		password => "u9p7ilG2E6DycOfwKm27",
-  		require => Class["mysql::server"],
-	}
+    mysql::rights{"zabbix rights":
+      ensure   => present,
+      database => "zabbix",
+      user     => "zabbix",
+      password => "u9p7ilG2E6DycOfwKm27",
+      require => Class["mysql::server"],
+    }
 
-	exec { "importzabbixschema":
-		cwd => "/tmp",
-		command => "mysql -uroot zabbix < /usr/share/zabbix-server/mysql.sql",
-		refreshonly => true,
-		require => Mysql::Database["zabbix"],
-	}
+    exec { "importzabbixschema":
+      cwd => "/tmp",
+      command => "mysql -uroot zabbix < /usr/share/zabbix-server/mysql.sql",
+      refreshonly => true,
+      require => Mysql::Database["zabbix"],
+    }
 
-	exec { "importzabbixdata":
-		cwd => "/tmp",
-		command => "mysql -uroot zabbix < /usr/share/zabbix-server/data.sql",
-		refreshonly => true,
-		require => Exec["importzabbixschema"],
-	}
+    exec { "importzabbixdata":
+      cwd => "/tmp",
+      command => "mysql -uroot zabbix < /usr/share/zabbix-server/data.sql",
+      refreshonly => true,
+      require => Exec["importzabbixschema"],
+    }
 
 }
