cce5c6a5f42d63d1fca611339e9cd613daa43469
Tom Barber <tom@analytical-labs.com>
2012-11-07 13:03:31 +0000

    mysql monitoring

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -18,7 +18,7 @@ class zabbix::agent {
 	$zabbix_register_sh = "/etc/zabbix/zabbix_register.sh"
 	$zabbix_agent_conf = "/etc/zabbix/zabbix_agent.conf"
 	package {
-		"zabbix-agent" :
+		["zabbix-agent", "libio-all-lwp-perl", "LWP.pm"] :
 			ensure => installed ;
 	}
 	service {
@@ -50,4 +50,25 @@ class zabbix::agent {
 			content => template("zabbix/zabbix_agentd_conf.erb"),
 			require => Package["zabbix-agent"] ;
 	}
+	
+	file{"/usr/local/mysql_performance_monitor_agent-0.9.tar.gz":
+	  ensure => present,
+	  source => "puppet:///modules/zabbix/mysql_performance_monitor_agent-0.9.tar.gz"
+	} ->
+	 exec { "extract":
+    command => "tar xvfz mysql_performance_monitor_agent-0.9.tar.gz",
+    creates => "/usr/local/mysql_performance_monitor_agent-0.9",
+    user => "root",
+    path    => "/usr/bin/:/bin/:/home/ingres/ingres/bin:/home/ingres/ingres/utility",    
+  } ->
+  file { "/usr/local/mysql_performance_monitor":
+    ensure => link,
+    source => "/usr/local/mysql_performance_monitor_agent-0.9"
+    
+  }-> 
+  file {"/usr/local/mysql_performance_monitor/etc/FromDualMySQLagent.conf":
+    ensure => present,
+    content => template("FromDualMySQLagent.conf.template.erb"),
+    
+  }
 }
